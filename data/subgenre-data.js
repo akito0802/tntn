f7a3f40ba969bbe3a81e807eda936d6c8fa1a59f
akito0802 <light.of.king.a@gmail.com>
2025-07-01 09:48:09 +0900

    Update subgenre-data.js

--- data/subgenre-data.js
+++ data/subgenre-data.js
@@ -1,13 +1,18 @@
 const genreData = {
-  "スウィング系": {
-    "スウィング": {
-      "description": "1930年代を代表するビッグバンド形式のジャズ。ダンス音楽として世界中で流行。",
-      "artists": ["ベニー・グッドマン", "カウント・ベイシー", "デューク・エリントン"],
-      "songs": ["Sing, Sing, Sing - Benny Goodman", "One O'Clock Jump - Count Basie"],
+  "トラディショナル / 初期ジャズ": {
+    "ニューオーリンズジャズ": {
+      "description": "",
+      "artists": [],
+      "songs": [],
       "japanese": {
-        "artists": ["原信夫とシャープス＆フラッツ"],
-        "songs": ["スウィング・ジャズ・メドレー"]
+        "artists": [],
+        "songs": []
       }
-    }
-  }
+    },
+    "ディキシーランド": {
+      ...
+    },
+    ...
+  },
+  ...
 };
